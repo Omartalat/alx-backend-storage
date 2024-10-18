aa8e563809fc98d5896ca5d11863aec27ed96738
dr.omartalat <dr.omartalat@gmail.com>
2024-10-18 15:14:29 +0300

    users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,12 @@
+
+-- This script creates a table named 'users' if it does not already exist.
+-- The 'users' table has the following columns:
+-- - id: An integer that auto-increments and serves as the primary key.
+-- - email: A varchar field with a maximum length of 255 characters, which must be unique and cannot be null.
+-- - name: A varchar field with a maximum length of 255 characters, which can be null.
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+        PRIMARY KEY(id)
+    );

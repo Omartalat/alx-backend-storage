0aa7f0ca2c9b11c953657e5aea1803c487856b87
dr.omartalat <dr.omartalat@gmail.com>
2024-10-18 16:27:51 +0300

    glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,7 @@
+-- This SQL query retrieves the band names and their lifespans for bands that play Glam rock.
+-- The lifespan is calculated as the difference between the year the band was formed and the year they split.
+-- If the band has not split, the lifespan is calculated up to the year 2022.
+-- The results are ordered by lifespan in descending order.
 SELECT band_name, 
 CASE
     WHEN split IS NULL THEN 2022 - formed
